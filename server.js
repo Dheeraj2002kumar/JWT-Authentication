5aa15ec46cea18648f3315faece54474bde8f9e3
Dheeraj Kumar <dk8549644@gmail.com>
2025-07-06 12:15:21 +0530

    Implement JWT authentication with access and refresh tokens; add authServer.js and update server.js and requests.rest

--- server.js
+++ server.js
@@ -1,16 +1,21 @@
+require('dotenv').config()
+
+
 // index.js
 const express = require('express');
+const jwt = require('jsonwebtoken');
+
 const app = express();
 const PORT = 3000;
 
 app.use(express.json())
 const posts = [
   {
-    username: 'Kyle',
+    username: 'Dheeraj',
     title: 'Post 1'
   },
   {
-    username: 'Jim',
+    username: 'Niharika',
     title: 'Post 2'
   }
 ]
@@ -20,10 +25,24 @@ app.get('/', (req, res) => {
   res.send('Hello, world!');
 });
 
-app.get('/posts', (req, res) => {
-    res.json(posts)
+app.get('/posts', authenticateToken, (req, res) => {
+  res.json(posts.filter(post => post.username === req.user.name));
 })
 
+
+// Authenticate token middelware
+function authenticateToken(req, res, next) {
+  const authHeader = req.headers['authorization']
+  const token = authHeader && authHeader.split(' ')[1];
+  if (token == null) return res.sendStatus(401);
+
+  jwt.verify(token, 'process.env.ACCESS_TOKEN_SECRET', (err, user) => {
+    console.log(err)
+    if (err) return res.sendStatus(403);
+    req.user = user;
+    next();
+  })
+}
 
 // Start the server
 app.listen(PORT, () => {
